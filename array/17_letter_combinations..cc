b1b9f89e3e1f45727d5caea3566e289dfa3a3947
hongdazhu <hongdazhubcn@gmail.com>
2024-08-03 23:44:06 +0200

    feat: 17

---
+++ array/17_letter_combinations..cc
@@ -0,0 +1,37 @@
+class Solution {
+private:
+        const string letterMap[10] = {
+            "", // 0
+            "", // 1
+            "abc", // 2
+            "def", // 3
+            "ghi", // 4
+            "jkl", // 5
+            "mno", // 6
+            "pqrs", // 7
+            "tuv", // 8
+            "wxyz", // 9
+        };
+public:
+    vector<string> result;
+    void getCombinations(const string& digits, int index, const string& s) { 
+        if (index == digits.size()) {
+            result.push_back(s);
+            return;
+        }
+        int digit = digits[index] - '0';
+        string letters = letterMap[digit];
+        for (int i = 0; i < letters.size(); i++) {
+            getCombinations(digits, index + 1, s + letters[i]);  
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        result.clear();
+        if (digits.size() == 0) {
+            return result;
+        }
+        getCombinations(digits, 0, "");
+        return result;
+
+    }
+};
